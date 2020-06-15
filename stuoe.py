ac05aa8165e5c7c293c83bf9750ae841e551406f
snbck <snbck@vip.qq.com>
2020-06-15 19:29:14 +0800

    一点

--- stuoe.py
+++ stuoe.py
@@ -1,6 +1,7 @@
 # Stuoe Start or Installing
 
 from flask import *
+from stuoe.route import *
 import flask_mail
 import os
 import time
@@ -8,6 +9,14 @@ import time
 # Get Configs File
 serverconf = dict(eval(open('server.conf', 'rb').read()))
 
-# Check Server init State
-if not serverconf['init']:
-    pass
+# Init Flask
+app = Flask(__name__)
+
+init_route(app)
+
+
+@app.route('/install')
+def installing_p():
+    return ''
+
+app.run(port=80, debug=True)
