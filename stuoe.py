17d9b9eb52e3b9722b5932d9c84c884f97afaab2
snbck <snbck@vip.qq.com>
2020-06-16 20:52:00 +0800

    一个debugger

--- stuoe.py
+++ stuoe.py
@@ -27,7 +27,7 @@ def installing_step(url):
         open('serverconf','wb+').write(str(serverconf).encode('utf-8'))
         return redirect('/install/database')
 
-    elif url == 'smtp' and request.method="POST":
+    elif url == 'smtp' and request.method=="POST":
         serverconf['info']['stuoe_name'] = request.form['fourm_name']
         serverconf['info']['des'] = request.form['fourm_admin_email']
         serverconf['info']['fourm_des'] = request.form['fourm_des']
@@ -40,4 +40,25 @@ def installing_step(url):
     elif url == "smtp" and request.method=="GET":
         return open('/storage/dist/installing/smtp.html','rb').read()
 
+@app.route('/js/<path:path>')
+def send_jsfile(path):
+    if os.path.exists("storage/dist/js/" + path):
+        return open("storage/dist/js/" + path,'rb').read()
+    else:
+        return abort(404)
+
+@app.route('/css/<path:path>')
+def send_cssfile(path):
+    if os.path.exists("storage/dist/css/" + path):
+        return open("storage/dist/css/" + path,'rb').read()
+    else:
+        return abort(404)
+
+@app.route('/')
+def send_vuejsindex():
+    return open("storage/dist/index.html",'rb').read()
+    
+
 app.run(port=80, debug=True)
+
+
