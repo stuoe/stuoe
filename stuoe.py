a5eef054ae96e21c1499fb3a08c12d48f4413f86
snbck <snbck@vip.qq.com>
2020-06-17 19:33:53 +0800

    把element添加到项目

--- stuoe.py
+++ stuoe.py
@@ -4,6 +4,7 @@ from flask import *
 import flask_mail
 import os
 import time
+import jinja2
 
 # Get Configs File
 serverconf = dict(eval(open('server.conf', 'rb').read()))
@@ -25,25 +26,32 @@ def installing_step(url):
         serverconf['info']['fourm_des'] = request.form['fourm_des']
         open('serverconf','wb+').write(str(serverconf).encode('utf-8'))
         return redirect('/install/database')
-
-    elif url == 'smtp' and request.method=="POST":
-        serverconf['info']['stuoe_name'] = request.form['fourm_name']
-        serverconf['info']['des'] = request.form['fourm_admin_email']
-        serverconf['info']['fourm_des'] = request.form['fourm_des']
-        open('serverconf','wb+').write(str(serverconf).encode('utf-8'))
-        return redirect('/install/installing')
     elif url == 'database' and request.method=="POST":
-        serverconf['info']['stuoe_name'] = request.form['fourm_name']
-        serverconf['info']['des'] = request.form['fourm_admin_email']
-        serverconf['info']['fourm_des'] = request.form['fourm_des']
+        serverconf['database']['host'] = request.form['host']
+        serverconf['database']['port'] = request.form['port']
+        serverconf['database']['db_name'] = request.form['db_name']
+        serverconf['database']['db_user'] = request.form['db_name']
+        serverconf['database']['db_password'] = request.form['db_password']
         open('serverconf','wb+').write(str(serverconf).encode('utf-8'))
         return redirect('/install/smtp')
+    elif url == 'smtp' and request.method=="POST":
+        serverconf['database']['host'] = request.form['host']
+        serverconf['database']['port'] = request.form['port']
+        serverconf['database']['db_name'] = request.form['db_name']
+        serverconf['database']['db_user'] = request.form['db_name']
+        serverconf['database']['db_password'] = request.form['db_password']
+        open('serverconf','wb+').write(str(serverconf).encode('utf-8'))
+        return redirect('/install/smtp')
+    
     elif url == "start" and request.method=="GET":
         return open('backUp/templates/installing/start.html','rb').read()
     elif url == "database" and request.method=="GET":
         return open('backUp/templates/installing/database.html','rb').read()
     elif url == "smtp" and request.method=="GET":
         return open('backUp/templates/installing/smtp.html','rb').read()
+    elif url == "installing" and request.method=="GET":
+        pass
+        
 
 @app.route('/js/<path:path>')
 def send_jsfile(path):
