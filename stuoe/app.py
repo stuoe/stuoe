3fe13f62b4af7f2b10a2b36265586f25b9ef6092
snbck <snbckcode@gmail.com>
2020-07-28 20:59:12 +0800

    A Release is v0.1.4

--- stuoe/app.py
+++ stuoe/app.py
@@ -34,7 +34,7 @@ import view
 # Global Var
 verify_registered_email = list()
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
-Release = 'v0.1.3.9 Release'
+Release = 'v0.1.4 Release'
 
 
 # Get Configs File
